60689fe2809d6ac658dc94c1eb0884544301edd1
sunxiaozhe007 <985160269@qq.com>
2018-07-25 15:55:51 +0800

    历年学生成绩持久化以及最近一学期成绩获取

--- src/main/java/com/xiyou3g/xiyouhelper/web/service/impls/AchievementService.java
+++ src/main/java/com/xiyou3g/xiyouhelper/web/service/impls/AchievementService.java
@@ -1,11 +1,18 @@
 package com.xiyou3g.xiyouhelper.web.service.impls;
 
+import com.xiyou3g.xiyouhelper.common.ServerResponse;
+import com.xiyou3g.xiyouhelper.dao.AchievementMapper;
 import com.xiyou3g.xiyouhelper.model.Achievement;
+import com.xiyou3g.xiyouhelper.model.NewAchievement;
 import com.xiyou3g.xiyouhelper.processor.AchievementProcessor;
+import com.xiyou3g.xiyouhelper.processor.BoxProcess;
+import com.xiyou3g.xiyouhelper.processor.HiddenProcessor;
 import com.xiyou3g.xiyouhelper.web.service.IAchievementService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -14,8 +21,52 @@ public class AchievementService implements IAchievementService {
     @Autowired
     private AchievementProcessor achievementProcessor;
 
+    @Autowired
+    private BoxProcess process;
+
+    @Autowired
+    private HiddenProcessor hiddenProcessor;
+
+    @Resource
+    private AchievementMapper achievementMapper;
+
+
     @Override
-    public List<Achievement> getAchievement(String name,String num,String sessionId,String year,String semester) {
-        return achievementProcessor.start(name,num,sessionId,year,semester);
+    public void getAchievement(String name, String num, String sessionId) {
+
+        String value3 = hiddenProcessor.start(name,num,sessionId);
+        System.out.println(value3);
+        //爬取所有学年
+        List<String> lists = process.getBox(name,num,sessionId);
+
+
+        for (String list : lists){
+            for (int i = 1;i<=2;i++){
+                List<Achievement> achievements = achievementProcessor.start(name,num,sessionId,list, String.valueOf(i),value3);
+                for (Achievement achievement : achievements){
+                    achievementMapper.insertAchievement(achievement);
+                }
+            }
+        }
+
+    }
+
+    @Override
+    public NewAchievement getNewAchievement(String name, String num, String sessionId) {
+        String value3 = hiddenProcessor.start(name,num,sessionId);
+        //爬取所有学年
+        List<String> lists = process.getBox(name,num,sessionId);
+        List<Achievement> achievements;
+        NewAchievement newAchievement = new NewAchievement();
+        newAchievement.setYear(lists);
+        achievements = achievementProcessor.start(name,num,sessionId,lists.get(0),"2",value3);
+        if (achievements.size() == 0){
+            achievements = achievementProcessor.start(name,num,sessionId,lists.get(0),"1",value3);
+            newAchievement.setAchievements(achievements);
+            return newAchievement;
+        }else {
+            newAchievement.setAchievements(achievements);
+            return newAchievement;
+        }
     }
 }
